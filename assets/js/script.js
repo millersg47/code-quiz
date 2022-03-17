7d26c141d1a0ce0c805ace20acc2f245cac89aea
Sophie Miller <millersg47@gmail.com>
2022-03-17 08:45:57 -0700

    adds startGame function, updates renderQA function and updates variables throughout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//defining globabl variables based on original html elements that will generate events
+//defining globabl variables based on original html elements that will generate events or be manipulated by an event
 var scores = document.querySelector(".scores");
 var timerEl = document.querySelector(".timer-value");
 var startButton = document.querySelector(".start");
@@ -12,17 +12,17 @@ var timer;
 var timerCount;
 
 
-//declares variable as object containing question 1 and answers
-var quiz1 = {
-    question: "This is the first question",
-    answers: ["answer1", "answer2", "answer3", "answer4"],
-}
-//declares variable as object containing question 2 and answers
-var quiz2 = {
-    question: "This is the second question",
-    answers: ["answer1", "answer2", "answer3", "answer4"],
-}
-
+//declares variable for quiz q&a content as array containing an object for each question and answer set
+var quiz = [ 
+  {
+    question1: "This is the first question",
+    answers1: ["answer1", "answer2", "answer3", "answer4"],  
+  },
+  {
+    question2: "This is the second question",
+    answers2: ["answer1", "answer2", "answer3", "answer4"],
+  }
+]
 // The init function is called when the page loads 
 function init() {
     pullWins();
@@ -30,9 +30,9 @@ function init() {
   }
 // startGame function initiates timercountdown and renderQA1 functions; used in startButton event listener
 function startGame() {
-    timerCount = 60;
+    timerCount = 6;
     startTimer()
-    renderQA1()
+    // renderQA()
 };
 
 // The startTimer function starts timer and stops it when timerCount reaches 0
@@ -41,17 +41,37 @@ function startTimer() {
     timer = setInterval(function() {
       timerCount--;
       timerEl.textContent = timerCount;
-      if (timerCount <= 0) {
+      if (timerCount === 0) {
+       gameOver(); 
+      }
+      else if (timerCount <= 0) {
         timerEl.textContent = " ";
         clearInterval(startTimer);
       }
     }, 1000);
   };
 
+// gameOver function is called when all answers have been collected and when timer reaches 0. It loads a form submission page to enter intials for high scores
+function gameOver() {
+  var gameOverEl = document.createElement("h2");
+  var scoreTextEl = document.createElement("p");
+  var nameFormContainer = document.createElement("form");
+  var nameForm = document.createElement("label");
+  var nameInput = document.createElement("input");
+  gameOverEl.textContent = "Game Over!"
+  scoreTextEl.textContent = "Your score is "; //need to add + and var for dynamic score # based on number of questions answered correct
+  nameForm.textContent = "Enter your initials to be added to high scores";
+  nameFormContainer.appendChild(nameForm, nameInput);
+  // is there a way to remove or append multiple elements? 
+  content.removeChild(text, startButton, answerList, result);
+  content.appendChild(gameOverEl, scoreTextEl, nameFormContainer);
+  
+  }
 //render QA1 removes intro text and presents first question and set of answers to user
-function renderQA1() {
+function renderQA() {
     content.removeChild(startButton);
-    prompt.textContent = quiz1.question;
+    //is syntax correct in below? trying to load the first question in place of the intro text
+    prompt.textContent = quiz.question1;
     //below line flagging an error on console, answerList.createElement is not a function
     var answ1 = answerList.createElement("li");
     answ1.textContent = quiz1.answers[0];
